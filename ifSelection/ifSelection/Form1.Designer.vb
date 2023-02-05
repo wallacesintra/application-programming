78eeb5cfbb1eec42b283e1ac232ee7bd7a6c44da
wallacelliot <103658056+wallacesintra@users.noreply.github.com>
2023-02-05 14:15:09 +0300

    vb

---
+++ ifSelection/ifSelection/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.labelCountry = New System.Windows.Forms.Label()
+        Me.txtCountry = New System.Windows.Forms.TextBox()
+        Me.btnGreet = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'labelCountry
+        '
+        Me.labelCountry.AutoSize = True
+        Me.labelCountry.Location = New System.Drawing.Point(40, 38)
+        Me.labelCountry.Name = "labelCountry"
+        Me.labelCountry.Size = New System.Drawing.Size(77, 13)
+        Me.labelCountry.TabIndex = 0
+        Me.labelCountry.Text = "Enter Country :"
+        '
+        'txtCountry
+        '
+        Me.txtCountry.Location = New System.Drawing.Point(169, 38)
+        Me.txtCountry.Name = "txtCountry"
+        Me.txtCountry.Size = New System.Drawing.Size(178, 20)
+        Me.txtCountry.TabIndex = 1
+        '
+        'btnGreet
+        '
+        Me.btnGreet.Location = New System.Drawing.Point(169, 91)
+        Me.btnGreet.Name = "btnGreet"
+        Me.btnGreet.Size = New System.Drawing.Size(75, 23)
+        Me.btnGreet.TabIndex = 2
+        Me.btnGreet.Text = "greetings"
+        Me.btnGreet.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(508, 261)
+        Me.Controls.Add(Me.btnGreet)
+        Me.Controls.Add(Me.txtCountry)
+        Me.Controls.Add(Me.labelCountry)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents labelCountry As System.Windows.Forms.Label
+    Friend WithEvents txtCountry As System.Windows.Forms.TextBox
+    Friend WithEvents btnGreet As System.Windows.Forms.Button
+
+End Class
---
+++ ifSelection/ifSelection/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGreet.Click
+        Dim country As String
+        country = txtCountry.Text
+
+        If country = "Kenya" Then
+            MsgBox("Habari")
+        ElseIf country = "England" Then
+            MsgBox("Hello")
+        Else
+            MsgBox("Good Morning")
+
+        End If
+    End Sub
+End Class
